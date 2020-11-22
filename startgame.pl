487250e02cce8a687f4858c7476dce9256f6e2a0
hafidabi <abi.daniswara@gmail.com>
2020-11-22 17:02:13 +0700

    perbaikan pada startgame.pl

--- startgame.pl
+++ startgame.pl
@@ -2,28 +2,28 @@
 enginestats(0).
 start :-
     enginestats(1),
-    write("in game situation"),nl,!;
-    write("selamat datang di tokemon"),nl,
-    write("ketikkan help untuk bantuan"),nl,
-    write("ketikkan start sekali lagi untuk mulai game"),nl,
+    write('in game situation'),nl,!;
+    write('selamat datang di tokemon'),nl,
+    write('ketikkan help untuk bantuan'),nl,
+    write('ketikkan start sekali lagi untuk mulai game'),nl,
     asserta(enginestats(1)),!.
 
     
 
 help :-
-    write("daftar perintah : "),nl,
-    write("w untuk maju "),nl,ss
-    write("a untuk kanan "),nl,
-    write("s untuk mundur "),nl,
-    write("d untuk kiri "),nl,
-    write(" "),nl,
-    write(" "),nl,
-    write(" "),nl,
-    write(" "),nl.
+    write('daftar perintah : '),nl,
+    write('w untuk maju'),nl,
+    write('a untuk kanan'),nl,
+    write('s untuk mundur'),nl,
+    write('d untuk kiri'),nl,
+    write(''),nl,
+    write(''),nl,
+    write(''),nl,
+    write(''),nl.
 
 
 
 
 quit:-
-    write("thanks for using this program"),nl,
-    write("arigatouuu"),nl.
+    write('thanks for using this program'),nl,
+    write('arigatouuu'),nl.
