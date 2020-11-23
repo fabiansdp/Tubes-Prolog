13784e4f3263de1de63761bff43075e8e801c3ef
syamilca <syamilca01@gmail.com>
2020-11-23 16:06:29 +0700

    update enginestats

--- move.pl
+++ move.pl
@@ -1,5 +1,11 @@
 :- include('map.pl').
 
+
+
+w :-
+    \+ enginestats(1),
+    write('mulai sek mas'), nl, !.
+
 w :-
     player_position(_,Y),
     Y is 1, write('duar tembok'), nl, !.
@@ -60,8 +66,7 @@ w :-
     retract(player_position(X,Y)),
     asserta(player_position(X2,Y2)),
     write('dziingg, teleport nih'), nl,
-    teleport,
-    print_all, !.
+    teleport, !.
 
 w :-
     player_position(X,Y),
@@ -92,8 +97,11 @@ w :-
     Y2 is Y-1,
     X2 is X,
     retract(player_position(X,Y)),
-    asserta(player_position(X2,Y2)),
-    print_all, !.
+    asserta(player_position(X2,Y2)), !.
+
+a :-
+    \+ enginestats(1),
+    write('mulai sek mas'), nl, !.
 
 a :-
     player_position(X,_),
@@ -155,8 +163,7 @@ a :-
     retract(player_position(X,Y)),
     asserta(player_position(X2,Y2)),
     write('dziingg, teleport nih'), nl,
-    teleport,
-    print_all, !.
+    teleport, !.
 
 a :-
     player_position(X,Y),
@@ -187,8 +194,11 @@ a :-
     X2 is X-1,
     Y2 is Y,
     retract(player_position(X,Y)),
-    asserta(player_position(X2,Y2)),
-    print_all, !.
+    asserta(player_position(X2,Y2)), !.
+
+s :-
+    \+ enginestats(1),
+    write('mulai sek mas'), nl, !.
 
 s :-
     player_position(_,Y),
@@ -267,8 +277,7 @@ s :-
     retract(player_position(X,Y)),
     asserta(player_position(X2,Y2)),
     write('dziingg, teleport nih'), nl,
-    teleport,
-    print_all, !.
+    teleport, !.
 
 s :-
     player_position(X,Y),
@@ -276,8 +285,11 @@ s :-
     Y2 is Y+1,
     X2 is X,
     retract(player_position(X,Y)),
-    asserta(player_position(X2,Y2)),
-    print_all, !.
+    asserta(player_position(X2,Y2)), !.
+
+d :-
+    \+ enginestats(1),
+    write('mulai sek mas'), nl, !.
 
 d :-
     player_position(X,_),
@@ -356,8 +368,7 @@ d :-
     retract(player_position(X,Y)),
     asserta(player_position(X2,Y2)),
     write('dziingg, teleport nih'), nl,
-    teleport,
-    print_all, !.
+    teleport, !.
     
 d :-
     player_position(X,Y),
@@ -365,8 +376,7 @@ d :-
     X2 is X+1,
     Y2 is Y,
     retract(player_position(X,Y)),
-    asserta(player_position(X2,Y2)),
-    print_all, !.
+    asserta(player_position(X2,Y2)), !.
 
 
 teleport :-
@@ -396,3 +406,6 @@ encounter :-
 encounter :-
     player_position(X,Y),
     X > 12, battle(3), !.
+
+obtain_treasure :-
+    treasure_status(1).
